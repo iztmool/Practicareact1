ccb504d5a15342d2acf0d56a7cf2b86ef9fefe41
iztmool <Jonathan.Navarro@tectijuana.edu.mx>
2020-10-27 16:12:47 -0700

    Updates

---
+++ precache-manifest.9626d1403f9f2f0b81e878f0eb21b5c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32303eee8d52e0d6996880cb37a0d2da",
+    "url": "/Practicareact1/index.html"
+  },
+  {
+    "revision": "96dab5266b5210f5c755",
+    "url": "/Practicareact1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad01db8f7dece61070a1",
+    "url": "/Practicareact1/static/js/2.79d9f4ea.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Practicareact1/static/js/2.79d9f4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dab5266b5210f5c755",
+    "url": "/Practicareact1/static/js/main.0eaed925.chunk.js"
+  },
+  {
+    "revision": "ff45eb30e4c8efb1b1dd",
+    "url": "/Practicareact1/static/js/runtime-main.cd8c6c04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Practicareact1/static/media/logo.5d5d9eef.svg"
+  }
+]);
